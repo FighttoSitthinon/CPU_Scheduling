27022211b2cde0293dacaa72f96a86a7e9d79e36
Sitthinon Chanaritthichai <31182493+FighttoSitthinon@users.noreply.github.com>
2018-02-28 20:49:25 +0700

    Fix Error

--- CPU_Scheduling/MainWindow.xaml.cs
+++ CPU_Scheduling/MainWindow.xaml.cs
@@ -21,11 +21,11 @@ namespace CPU_Scheduling
         
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Obj obj = new Obj();
+            Obj obj = new Obj(null, 0, 0, 0, 0);
             string url = inputBox.Text;
             string[] lines = System.IO.File.ReadAllLines(url);
             string[] word;
-            output.Content = "Input";
+            output.Content = "Process" + "\t" +"Arrive"+ "\t" +"Brust" + "\t" +"Priority" + "\t" +"Quantumm";
             foreach (string line in lines)
             {
                 word = line.Split(' ');
@@ -34,7 +34,7 @@ namespace CPU_Scheduling
                 obj.BrustTime = Int32.Parse(word[2]);
                 obj.Priority = Int32.Parse(word[3]);
                 obj.Quantumm = Int32.Parse(word[4]);
-                output.Content += "\n" + obj.Process;
+                output.Content +=  "\n" + obj.Process +"\t"+ obj.ArriveTime + "\t" + obj.BrustTime + "\t" + obj.Priority + "\t" + obj.Quantumm ;
             }
         }
     }
