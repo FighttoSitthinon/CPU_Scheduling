e9bc129849c21da8062f5aafa644c1f4415b9f73
Niracha S. <meenniiz123@hotmail.com>
2018-03-04 06:28:25 +0000

    Updated RR.cs

--- CPU_Scheduling/Controllers/RR.cs
+++ CPU_Scheduling/Controllers/RR.cs
@@ -8,5 +8,59 @@ namespace CPU_Scheduling.Controllers
 {
     class RR
     {
+        
+        public string SortProcess(List<Obj> objList)
+        {
+            
+            string Output = "\t";
+            int timeuse = 0;
+            int timetotal = 0;
+            int retime = 0;
+           
+            
+            foreach (var i in objList)
+            {
+                                 
+                
+                Obj newTask = new Obj();
+                
+                if (i.ArriveTime <= timetotal)
+                {
+                    if (i.BrustTime == i.Quantumm)
+                    {
+                        timeuse = i.BrustTime;
+                        newTask.Process += i.Process;
+
+                    }
+                    else if (i.BrustTime > i.Quantumm)
+                    {
+                        timeuse = i.Quantumm;
+                        retime = i.BrustTime - i.Quantumm;
+                        newTask.Process += i.Process;
+
+
+                    }
+                    else if (i.BrustTime < i.Quantumm)
+                    {
+                        timeuse = i.BrustTime;
+                        newTask.Process += i.Process;
+
+
+                    }
+
+                    timetotal += timeuse;
+                  
+                }
+                Output += "\t" + newTask.Process + "-" + timetotal;
+                
+
+
+
+
+
+
+            }
+            return Output;
+        }
     }
 }
